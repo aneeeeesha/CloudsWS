88092f810093fa28e6280a0ca9d2dab3ec2cf590
Aneesh015 <aneepundoor@gmail.com>
2023-07-27 16:12:07 +0530

    Contains a basic get command

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const app = require("express");
+const app = require("express")();
+
+
+app.get("/",(req,res)=>{
+    res.send("Helloo");
+});
+
+app.listen(3000,()=>{
+    console.log("Listening at the port 3000");
+})
